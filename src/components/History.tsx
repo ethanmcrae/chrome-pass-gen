dda696f7da9f07535d166c237db7c7799fc69e1e
ethanmcrae <ethanmcrae0@gmail.com>
2023-04-28 03:44:04 -0600

    Dark mode with purple palette

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -43,24 +43,24 @@ const History: React.FC<HistoryProps> = ({ passwordHistory, setPasswordHistory, 
 
   return (
     <div className="flex flex-col items-center mt-4">
-      <button className="text-blue-600" onClick={() => setVisibleHistory(!visibleHistory)}> {visibleHistory ? 'Hide' : 'Show'} History</button>
+      <button className="text-customPurple-100" onClick={() => setVisibleHistory(!visibleHistory)}> {visibleHistory ? 'Hide' : 'Show'} History</button>
       {visibleHistory && (
-        <ul className="w-full mt-4">
+        <ul className="w-full mt-4 flex flex-col items-center">
           {Object.entries(passwordHistory)
             .sort((a, b) => b[1].time - a[1].time)
             .map(([url, passwordData], index) => (
-              <li key={index} className="flex justify-between px-4 py-2 bg-white rounded shadow mb-2 gap-4">
+              <li key={index} className="flex justify-between items-center w-11/12 px-4 py-2 bg-customPurple-800 rounded-lg shadow mb-2 gap-4">
                 <span>{url}</span>
-                <span className="cursor-pointer monospaced py-1 px-2 bg-gray-500 bg-opacity-20" onClick={handleCopy}>{passwordData.password}</span>
+                <span className="cursor-pointer monospaced py-1 px-2 bg-gray-600 bg-opacity-20" onClick={handleCopy}>{passwordData.password}</span>
                 <button
                   onClick={newPassword}
-                  className="text-blue-600"
+                  className="text-customPurple-400"
                 >
                   <FontAwesomeIcon icon={faRefresh} />
                 </button>
                 <button
                   onClick={() => handleRemove(url)}
-                  className="text-red-600"
+                  className="text-red-800"
                 >
                   <FontAwesomeIcon icon={faTrash} />
                 </button>
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -15,19 +15,21 @@ const Settings: React.FC<SettingsProps> = ({ onCopy, includeSpecialChars, setInc
 
   return (
     <div className="flex mt-4 justify-around w-full">
-      <div className="w-1/2 flex flex-col items-center p-3">
-        <FontAwesomeIcon icon={faSync} className="text-white bg-blue-600 rounded-full p-4 text-2xl cursor-pointer" onClick={onCopy} />
+      <div className="w-1/2 flex flex-col justify-center items-center p-3 cursor-pointer" onClick={onCopy}>
+        <FontAwesomeIcon icon={faSync}
+        className="text-white rounded-full p-4 text-2xl"
+        style={{ background: "linear-gradient(315deg, rgba(127,87,180,1) 0%, rgba(135,87,180,1) 50%, rgba(142,87,180,1) 100%)"}} />
         <button className="mt-2 text-gray-600 cursor-default">
           Generate Again
         </button>
       </div>
       <div className="w-1/2 flex flex-col items-center p-3">
         <div className="w-full mb-2">
-          <label htmlFor="passwordLength" className="block text-xs">
+          <label htmlFor="passwordLength" className="block text-xs text-customPurple-200 ml-2">
             Length: {passwordLength}
           </label>
-          <div className="flex items-center bg-white py-3 px-2 rounded-md">
-            <span className="text-gray-700 text-xs">8</span>
+          <div className="flex items-center bg-gray-800 py-3 px-2 rounded-md">
+            <span className="text-customPurple-100 text-xs">8</span>
             <input
               id="passwordLength"
               type="range"
@@ -35,17 +37,17 @@ const Settings: React.FC<SettingsProps> = ({ onCopy, includeSpecialChars, setInc
               max="32"
               value={passwordLength}
               onChange={(e) => setPasswordLength(Number(e.target.value))}
-              className="w-full cursor-pointer mx-2 h-[2px] appearance-none bg-gray-200 focus:outline-none transition duration-150 ease-in-out thumb:white"
+              className="w-full cursor-pointer mx-2 h-[2px] appearance-none focus:outline-none transition duration-150 ease-in-out thumb:white"
               style={{
-                background: `linear-gradient(90deg, #8B5CF6 ${
+                background: `linear-gradient(90deg, #a372d0 ${
                   (passwordLength - 8) * 100 / 24
-                }%, #d1d5db ${(passwordLength - 8) * 100 / 24}%)`,
+                }%, #6b6570 ${(passwordLength - 8) * 100 / 24}%)`,
               }}
             />
-            <span className="text-gray-700 text-xs">32</span>
+            <span className="text-customPurple-100 text-xs">32</span>
           </div>
         </div>
-        <div className="w-full bg-white py-3 px-2 rounded-md">
+        <div className="w-full bg-gray-800 py-3 px-2 rounded-md">
           <AppleToggle state={includeSpecialChars} setState={setIncludeSpecialChars} label="Symbols" />
         </div>
       </div>
